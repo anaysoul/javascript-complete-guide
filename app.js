863d05c37d70645ead70b7688d16fd49479bbc0c
anaysoul <soulyana_lakew@outlook.com>
2022-03-11 10:23:06 -0500

    functions are objects!

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+console.dir(startGame);
+
+startGameBtn.addEventListener('click', startGame);
