ea7df130230015f443c37b8252f499b936f681cc
anaysoul <soulyana_lakew@outlook.com>
2022-03-12 16:28:51 -0500

    opening a modal by changing CSS classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.getElementById('add-modal');
 
 const startAddMovieButton = document.querySelector('header button');
 
-const addModal = (addMovieModal) => {
-  addMovieModal.className = 'modal visible';
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
 };
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
