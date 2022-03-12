3491e5183da7961e6a5a9b6c80034d731872108f
anaysoul <soulyana_lakew@outlook.com>
2022-03-12 16:23:57 -0500

    selecting the modal and 'add' button

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const addMovieModal = document.getElementById('add-modal');
+
+const startAddMovieButton = document.querySelector('header button');
+
+const addModal = (addMovieModal) => {
+  addMovieModal.className = 'modal visible';
+};
