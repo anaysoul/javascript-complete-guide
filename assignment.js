b01e97459d27437422b3fbf4a57af5db7a787d48
anaysoul <soulyana_lakew@outlook.com>
2022-03-11 21:08:10 -0500

    assignment 1

---
+++ assignment.js
@@ -0,0 +1,16 @@
+const task1 = document.getElementById('task-1');
+task1.style.color = 'white';
+task1.style.backgroundColor = 'black';
+
+const task1a = document.body.querySelector('li');
+task1a.style.color = 'yellow';
+task1a.style.backgroundColor = 'grey';
+
+const task3 = document.querySelector('h1');
+task3.textContent = 'Assignment - Solved';
+
+const task2 = document.head.querySelector('title');
+task2.textContent = 'Assignment - Solved';
+
+const task2a = document.querySelector('title');
+task2a.textContent = 'ASSIGNMNET - SOLVED';
