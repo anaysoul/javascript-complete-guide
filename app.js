ca13d7fc7bb767a266c40bb22562e84bf2643a07
anaysoul <soulyana_lakew@outlook.com>
2022-03-12 21:08:37 -0500

    creating arrays

---
+++ app.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = new Array(5);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2, 3);
+// console.log(yetMoreNumbers);
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+const arrayListItems = Array.from(listItems); //split characters into an array
+console.log(arrayListItems);
