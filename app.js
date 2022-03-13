6b0a058f2981c4f7184126a2f2d60923929ae26b
anaysoul <soulyana_lakew@outlook.com>
2022-03-12 21:23:58 -0500

    push(), pop(), unshift(), shift() - Adding & Removing Elements

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const moreNumbers = new Array(5);
-// console.log(moreNumbers);
+// // const moreNumbers = new Array(5);
+// // console.log(moreNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2, 3);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2, 3);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const arrayListItems = Array.from(listItems); //split characters into an array
-console.log(arrayListItems);
+// const arrayListItems = Array.from(listItems); //split characters into an array
+// console.log(arrayListItems);
 
-const hobbies = ['cooking', 'coding', 'watching tv'];
-const personalData = [33, 'Soul', { moreDetail: [] }];
+// const hobbies = ['cooking', 'coding', 'watching tv'];
+// const personalData = [33, 'Soul', { moreDetail: [] }];
 
-const analyticsData = [
-  //multi-dimensonal arrays
-  [1, 1.6],
-  [-5.4, 2.1],
-];
+// const analyticsData = [
+//   //multi-dimensonal arrays
+//   [1, 1.6],
+//   [-5.4, 2.1],
+// ];
 
-for (const data of analyticsData) {
-  for (const dataPoint of data) {
-    console.log(dataPoint);
-  }
-}
+// for (const data of analyticsData) {
+//   for (const dataPoint of data) {
+//     console.log(dataPoint);
+//   }
+// }
+
+const hobbies = ['sports', 'cooking'];
+hobbies.push('coding');
+hobbies.unshift('reading');
+hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+console.log(hobbies);
+
+hobbies[5] = 'exercising';
+console.log(hobbies, hobbies[4]);
