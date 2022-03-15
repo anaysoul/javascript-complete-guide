648c5f5cd5e8a8be534c2c11297812e75f970b91
anaysoul <soulyana_lakew@outlook.com>
2022-03-14 20:35:12 -0400

    using & 'connecting multiple classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,43 +12,59 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+              <h2>${this.product.title}</h2>
+              <p>${this.product.description}</p>
+              <h3>\$${this.product.price}</h3>
+              <button>Add to Cart</button>
+          </div>
+        </div>
+        `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       'Pillow',
       'https://upload.wikimedia.org/wikipedia/commons/a/ad/Pillows_on_a_hotel_bed.jpg',
-      19.99,
-      'Soft pillows!'
+      'Soft pillows!',
+      19.99
     ),
     new Product(
       'Carpet',
       'https://upload.wikimedia.org/wikipedia/commons/c/cd/Polonaise_carpet_-_Kashan_%28Iran%29_-_17th_century_-_MET_-_Inventory_number_51.197.jpg',
-      89.99,
-      'A historic carpet!'
+      'A historic carpet!',
+      89.99
     ),
-  ],
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-          <div>
-            <img src="${prod.imageUrl}" alt="${prod.title}" >
-            <div class="product-item__content">
-                <h2>${prod.title}</h2>
-                <h3>\$${prod.price}</h3>
-                <p>${prod.description}</p>
-                <button>Add to Cart</button>
-            </div>
-          </div>
-          `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
